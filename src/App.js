8d0b2e132a3c44f22a3d91fe2d870e5a7bdc74eb
Carl Cortright <carl@carlcortright.com>
2022-03-10 11:30:30 -0500

    random logo and site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import { Flex, Heading } from 'rebass'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex sx={{
+        height: '100vh',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }}>
+        <Heading sx={{
+            fontFamily: 'Montserrat'
+        }}>
+            Polyweave
+        </Heading>
+    </Flex>
   );
 }
 
