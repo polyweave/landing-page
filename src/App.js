39ca20c63bc8998d0dee487248416a5eafbb57c2
Carl Cortright <carl@carlcortright.com>
2022-03-22 14:35:16 -0400

    redirect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { Flex, Heading } from 'rebass'
 
 function App() {
+  window.location.replace("https://polyweave.notion.site/Polyweave-0c385b22a30a41a0a9568d6f8c6f46a7");
   return (
     <Flex sx={{
         height: '100vh',
