fabcd7cec5637f00082b6707db5c6a81ed7fa576
Carl Cortright <carl@carlcortright.com>
2022-03-27 11:14:52 -0600

    new landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,186 @@
-import logo from './logo.svg';
-import { Flex, Heading } from 'rebass'
+import { Flex, Image, Link, Box, Heading, Button, Text } from 'rebass';
+import Terminal from "./Terminal";
+
+import logo from './Logo.png'
+import sectionLogo from './sectionLogo.png'
+
 
 function App() {
-  window.location.replace("https://polyweave.notion.site/Polyweave-0c385b22a30a41a0a9568d6f8c6f46a7");
-  return (
-    <Flex sx={{
-        height: '100vh',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }}>
-        <Heading sx={{
-            fontFamily: 'Montserrat'
-        }}>
-            Polyweave
-        </Heading>
-    </Flex>
-  );
+    return (
+        <Flex
+            sx={{
+                width: '100%',
+                justifyContent: 'center',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+            }}
+        >
+            {/* Navbar */}
+            <Flex
+                px={[3, 5]}
+                py={[3, 4]}
+                width={['100%', '900px']}
+                sx={{
+                    alignItems: 'center',
+                }}
+            >
+                <Image src={logo} sx={{ height: 100 }} />
+                <Box mx='auto' />
+                <Link
+                    sx={{
+                        color: 'black',
+                        margin: '20px',
+                        textDecoration: 'none'
+                    }}
+                    target={'_blank'}
+                    href={'https://polyweave.notion.site/polyweave/Polyweave-0c385b22a30a41a0a9568d6f8c6f46a7'}
+                >
+                    About
+                </Link>
+                {/*<Link*/}
+                {/*    sx={{*/}
+                {/*        color: 'black',*/}
+                {/*        textDecoration: 'none'*/}
+                {/*    }}*/}
+                {/*    target={'_blank'}*/}
+                {/*    href={'https://polyweave.notion.site/polyweave/Polyweave-0c385b22a30a41a0a9568d6f8c6f46a7'}*/}
+                {/*>*/}
+                {/*    Careers*/}
+                {/*</Link>*/}
+            </Flex>
+            {/* Header */}
+            <Flex
+                px={[3, 5]}
+                py={[3, 4]}
+                width={['100%', '900px']}
+            >
+                <H1Header width={'100%'} >Powerful APIs for Highly Contextual Blockchain Data</H1Header>
+            </Flex>
+            {/* Action button */}
+            <Flex
+                px={[3, 5]}
+                py={[3, 4]}
+                width={['100%', '900px']}
+                justifyContent={'center'}
+            >
+                <a href='https://airtable.com/shrr59NnMHu0ZiCED' target={'_blank'}>
+                    <Button
+                        sx={{
+                            backgroundColor: '#191814',
+                            fontSize: ['16px', '24px'],
+                            borderRadius: '50px',
+                            fontFamily: 'Montserrat',
+                            fontWeight: '600',
+                            ":hover": {
+                                cursor: 'pointer'
+                            }
+                        }}
+                    >
+                        Request Early Access
+                    </Button>
+                </a>
+            </Flex>
+            {/* Terminal */}
+            <Terminal />
+
+            {/* Info cards*/}
+            <Flex
+                px={[3, 5]}
+                py={[3, 4]}
+                width={['100%', '900px']}
+                justifyContent={'space-between'}
+                flexWrap={'wrap'}
+            >
+                <Flex
+                    sx={{
+                        width: ['100%', '250px'],
+                        flexWrap: 'wrap',
+                        alignContent: 'flex-start',
+                        my: ['20px', '0px']
+                    }}
+                >
+                    <Flex width={'100%'}>
+                        <Image src={sectionLogo} mb={'10px'} width={'35px'}/>
+                    </Flex>
+                    <H2Header>Human-readable Data</H2Header>
+                    <BodyText>Gone are the days of unreadable cryptic hashes. Human readable tagging and actually useful datapoints on everything</BodyText>
+                </Flex>
+                <Flex
+                    sx={{
+                        width: ['100%', '250px'],
+                        flexWrap: 'wrap',
+                        alignContent: 'flex-start',
+                        my: ['20px', '0px']
+                    }}
+                >
+                    <Flex width={'100%'}>
+                        <Image src={sectionLogo} mb={'10px'} width={'35px'}/>
+                    </Flex>
+                    <H2Header>Enterprise Grade Reliability</H2Header>
+                    <BodyText>Tight SLAs, built by the team that has shipped services to millions of users and served billions of requests</BodyText>
+                </Flex>
+                <Flex
+                    sx={{
+                        width: ['100%', '250px'],
+                        flexWrap: 'wrap',
+                        alignContent: 'flex-start',
+                        my: ['20px', '0px']
+                    }}
+                >
+                    <Flex width={'100%'}>
+                        <Image src={sectionLogo} mb={'10px'} width={'35px'}/>
+                    </Flex>
+                    <H2Header>Multichain by Default</H2Header>
+                    <BodyText>Easily integrate many chains. Fetch multichain balances and display NFT galleries cross chain</BodyText>
+                </Flex>
+            </Flex>
+
+            {/* Footer */}
+            <Flex
+                px={[3, 5]}
+                py={[3, 4]}
+                width={['100%', '900px']}
+                justifyContent={'center'}
+            >
+                <BodyText>Made with 🔨 by Polyweave</BodyText>
+            </Flex>
+        </Flex>
+    );
 }
+
+const H1Header = props =>
+    <Heading
+        {...props}
+        sx={{
+            fontFamily: 'Montserrat',
+            fontWeight: 'medium',
+            fontSize: [5, 6],
+            textAlign: 'center'
+        }}
+    />
+
+const H2Header = props =>
+    <Heading
+        {...props}
+        sx={{
+            fontFamily: 'Montserrat',
+            fontWeight: 'medium',
+            fontSize: '24px',
+            textAlign: 'left',
+            marginY: '10px'
+        }}
+    />
+
+const BodyText = props =>
+    <Text
+        {...props}
+        sx={{
+            fontFamily: 'Montserrat',
+            fontSize: 2,
+            textAlign: 'left',
+            color: '#242424',
+            fontSize: '18px',
+        }}
+    />
 
 export default App;
---
+++ src/Terminal.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Typed from 'typed.js'
+import {Flex, Text} from "rebass";
+
+class Terminal extends Component {
+    componentDidMount() {
+        const options = {
+            strings: ['curl https://api.polyweave.xyz/v1/solana/transactions?address=5ysepZzWuVQrkk74jGJhKFu3yevUVG93xQkqREkBaDVv'],
+            typeSpeed: 35,
+            backSpeed: 50,
+            showCursor: false,
+            onComplete: function(){
+                new Promise(r => setTimeout(r, 400)).then(() => {
+                    document.getElementById("json-response").style.visibility = "visible";
+                });
+            }
+        };
+        // this.el refers to the <span> in the render() method
+        this.typed = new Typed('.typing', options);
+    }
+
+    render() {
+        var json = "[{\"type\": \"nft_sale\", \"title\": \"Magic Eden Sale of Degod #4321\", \"amount\": 8.3, \"metadata\": {\"nft\": {\"rarity\": 481, \"floor_price\": 81.2, \"image_url\": \"api.polyweave.xyz/nfts/images/ahgyf2rxz.png\"}}},{\"type\": \"swap\", \"title\": \"Swap SOL for ORCA\", \"amount\": 321.8, \"metadata\": {\"swap\": {\"slippage\": 0.8}}}]"
+
+        return (
+            <Flex
+                px={[3, 5]}
+                py={[3, 4]}
+                width={['100%', '900px']}
+                justifyContent={'center'}
+            >
+                <Flex
+                    sx={{
+                        width: ['100%', '700px'],
+                        margin: '10px',
+                        minHeight: '300px',
+                        background: '#191814',
+                        borderRadius: '10px',
+                        flexDirection: 'column',
+                        flexWrap: 'wrap',
+                        alignItems: 'flex-start'
+                    }}
+                >
+                    <Flex
+                        sx={{
+                            height: '20px',
+                            background: '#efefef',
+                            width: '100%',
+                            borderRadius: '10px 10px 0px 0px',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Flex sx={{ backgroundColor: '#fb574e', width: '0.75rem', height: '0.75rem', borderRadius: '0.75rem', marginLeft: '8px'}}></Flex>
+                        <Flex sx={{ backgroundColor: '#fbbd2e', width: '0.75rem', height: '0.75rem', borderRadius: '0.75rem', marginLeft: '8px'}}></Flex>
+                        <Flex sx={{ backgroundColor: '#51e05d', width: '0.75rem', height: '0.75rem', borderRadius: '0.75rem', marginLeft: '8px'}}></Flex>
+                    </Flex>
+                    <Flex sx={{flex: 2, padding: '10px', width: '100%', flexDirection: 'column', flexWrap: 'wrap'}}>
+                        <Text sx={{fontFamily: 'B612 Mono', color: 'white', fontSize: '18px', width: '100%'}}>$ <span class={'typing'} style={ {overflowWrap: 'break-word'}}></span></Text>
+                        <br />
+                        <Text id={'json-response'} sx={{width: '100%', fontFamily: 'B612 Mono', color: 'white', fontSize: '18px', visibility: 'hidden'}}>{json}</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
+        );
+    }
+}
+
+
+
+export default Terminal;
